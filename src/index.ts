22c94a4a224955b743de2afaa6c3bd199882606c
jnm2 <me@jnm2.com>
2026-06-22 20:33:41 -0400

    Fix pointer offset

--- src/index.ts
+++ src/index.ts
@@ -470,7 +470,7 @@ canvas.onpointerdown = function(e) {
     if (e.button === 0) {
         parametersManager.hasCapture = true;
         canvas.setPointerCapture(e.pointerId);
-        addPoint(e.clientX, e.clientY);
+        addPoint(e.offsetX, e.offsetY);
     }
 };
 
@@ -481,7 +481,7 @@ canvas.ontouchstart = canvas.ontouchmove = function(e) {
 
 canvas.onpointermove = function(e) {
     if (parametersManager.hasCapture)
-        addPoint(e.clientX, e.clientY);
+        addPoint(e.offsetX, e.offsetY);
 };
 
 canvas.onpointerup = function(e) {
